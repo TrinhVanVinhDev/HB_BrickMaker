8f74b4e79bfc54dff5c27668a90db634ad523e6d
trinhvinhdev <trinhvinh95@gmail.com>
2023-11-17 13:42:27 +0700

    Update on MyPC

--- Assets/_Games/Scripts/Map/ListLevels.cs
+++ Assets/_Games/Scripts/Map/ListLevels.cs
@@ -1,7 +1,8 @@
+using System;
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.IO;
 
 public class ListLevels : MapManagement
 {
@@ -10,6 +11,15 @@ public class ListLevels : MapManagement
     private Vector2 pointRenderj;
     private Vector2 pointRenderi;
     private GameObject mapRended;
+
+    private float row;
+    private int itemInRow;
+
+    public static string textName
+    {
+        get { return textName; }
+        set { textName = value; }
+    }
     public override void Oninit()
     {
         base.Oninit();
@@ -20,18 +30,30 @@ public class ListLevels : MapManagement
     {
         FileInfo[] arrFiles = folderMap.GetFiles("*.txt");
         pointRenderj = pointRenderi = firstPoint;
-        for (int i = 0; i < 5; i++)
+
+        row = Mathf.Round(arrFiles.Length / 3);
+        int remainder;
+        int loop = 3;
+        int indexArrFile = 0;
+        float quotient = Math.DivRem(arrFiles.Length, 3, out remainder);
+        remainder = (remainder == 0) ? 3 : remainder;
+
+        for (int i = 0; i <= row; i++)
         {
-            for (int j = 0; j < 3; j++)
+            if(i == row)
+            {
+                loop = remainder;
+            }
+            for (int j = 0; j < loop; j++)
             {
                 mapRended = Instantiate(prefabsMaps, new Vector3(0f, 0f, 0f), transform.rotation);
-                Debug.Log(mapRended.gameObject.name);
                 mapRended.transform.SetParent(transform);
                 mapRended.GetComponent<RectTransform>().anchoredPosition = pointRenderj;
-                //mapRended.gameObject.GetComponentInChildren<>().text = arrFiles[i+j].Name;
+                textName = arrFiles[indexArrFile].Name;
                 pointRenderj = pointRenderj + new Rect(220f, 0, 0, 0).position;
+                indexArrFile++;
             }
-            pointRenderi = pointRenderi - new Rect(0, 480f, 0, 0).position;
+            pointRenderi = pointRenderi - new Rect(0, 240f, 0, 0).position;
             pointRenderj = pointRenderi;
         }
     }
---
+++ Assets/_Games/Scripts/Map/RenderTextMap.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderTextMap : MapManagement
+{
+    public override void Oninit()
+    {
+        base.Oninit();
+        
+    }
+}
